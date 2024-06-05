ec11ced22286133697cb6b5946c4d0d8c2d0e0d7
Ricardo Boss <mail@ricardoboss.de>
2024-06-06 00:49:29 +0200

    Added documentation for how IconThemeData is handled in CupertinoButton

--- packages/flutter/lib/src/cupertino/button.dart
+++ packages/flutter/lib/src/cupertino/button.dart
@@ -26,6 +26,10 @@ const EdgeInsets _kBackgroundButtonPadding = EdgeInsets.symmetric(
 /// [EdgeInsets.zero], should be used to prevent clipping larger [child]
 /// widgets.
 ///
+/// Preserves any parent [IconThemeData] but overwrites its [IconThemeData.color]
+/// with the [CupertinoThemeData.primaryColor] (or
+/// [CupertinoThemeData.primaryContrastingColor] if the button is disabled).
+///
 /// {@tool dartpad}
 /// This sample shows produces an enabled and disabled [CupertinoButton] and
 /// [CupertinoButton.filled].
